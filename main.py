cf031b4b077d7e9b98e85f5d5a2906cc9cdb9cda
wishmaestro <wishmaestro@gmail.com>
2021-03-09 22:28:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,23 @@ def create_app():
 
 app = create_app()
 
+
+@app.on_event("startup")
+async def start_up():
+    await admin_app.init(  # nosec
+        admin_secret="test",
+        permission=True,
+        admin_log=True,
+        site=Site(
+            name="FastAPI-Admin DEMO",
+            login_footer="FASTAPI ADMIN - FastAPI Admin Dashboard",
+            login_description="FastAPI Admin Dashboard",
+            locale="en-US",
+            locale_switcher=True,
+            theme_switcher=True,
+        ),
+    )
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, debug=False, reload=False, lifespan="on")
